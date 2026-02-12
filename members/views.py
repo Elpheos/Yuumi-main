924c46bd68f83d4234a83ccda78d80182b7ffcac
Elpheos <lucacardinal@gmail.com>
2026-02-12 11:28:04 +0100

    Fix indentation in return statement

--- members/views.py
+++ members/views.py
@@ -511,10 +511,11 @@ def by_super_category(request, departement, ville, super_slug):
             "image": image_url,
         })
 
- return render(request, "members/by_supercategory.html", {
+    return render(request, "members/by_supercategory.html", {
         "super_cat": super_cat,
         "categories": categories,
         "departement": departement,
         "ville": ville,
     })
+
 
