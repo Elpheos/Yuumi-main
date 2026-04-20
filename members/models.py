a5f390997a09fdc501e783181341940b927deec8
Elpheos <lucacardinal@gmail.com>
2026-04-20 18:52:48 +0200

    Update models.py

--- members/models.py
+++ members/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.core.validators import URLValidator
-from django.db import models
+
 
 
 # ===========================================================
