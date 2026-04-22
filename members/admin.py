db9898ca8754a2b1987e69657038491740363fe8
Elpheos <lucacardinal@gmail.com>
2026-04-22 13:52:24 +0200

    Add save_model method to update horaires timestamp

--- members/admin.py
+++ members/admin.py
@@ -129,7 +129,23 @@ class StoreAdmin(nested_admin.NestedModelAdmin):
             )
         return ""
     photo_preview.short_description = "Photo principale"
-
+    
+    def save_model(self, request, obj, form, change):
+            if any(
+                field in form.changed_data
+                for field in [
+                    'lundi_matin_ouverture', 'lundi_matin_fermeture', 'lundi_apresmidi_ouverture', 'lundi_apresmidi_fermeture',
+                    'mardi_matin_ouverture', 'mardi_matin_fermeture', 'mardi_apresmidi_ouverture', 'mardi_apresmidi_fermeture',
+                    'mercredi_matin_ouverture', 'mercredi_matin_fermeture', 'mercredi_apresmidi_ouverture', 'mercredi_apresmidi_fermeture',
+                    'jeudi_matin_ouverture', 'jeudi_matin_fermeture', 'jeudi_apresmidi_ouverture', 'jeudi_apresmidi_fermeture',
+                    'vendredi_matin_ouverture', 'vendredi_matin_fermeture', 'vendredi_apresmidi_ouverture', 'vendredi_apresmidi_fermeture',
+                    'samedi_matin_ouverture', 'samedi_matin_fermeture', 'samedi_apresmidi_ouverture', 'samedi_apresmidi_fermeture',
+                    'dimanche_matin_ouverture', 'dimanche_matin_fermeture', 'dimanche_apresmidi_ouverture', 'dimanche_apresmidi_fermeture',
+                ]
+            ):
+                obj.horaires_updated_at = timezone.now()
+            super().save_model(request, obj, form, change)
+    
     class Media:
         js = ("copy_horaires.js",)
 
