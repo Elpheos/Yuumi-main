f09ee6f24cd9062c79a33d05cf3c2d3f49901d26
Elpheos <lucacardinal@gmail.com>
2026-05-26 11:04:42 +0200

    Update views.py

--- members/views.py
+++ members/views.py
@@ -320,6 +320,7 @@ def search_product(request):
                 "store": store.nom,
                 "url": store.get_absolute_url(),
                 "photo": store.photo_small.url if store.photo_small else (store.photo.url if store.photo else ""),
+                 "address": store.addressemaps or "",
             })
 
     return JsonResponse({"results": results})
