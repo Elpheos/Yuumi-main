45c29a9b4f7e7798b74a2d1f4424351c9b2a325a
Elpheos <lucacardinal@gmail.com>
2026-06-21 16:26:16 +0200

    Update urls.py

--- members/urls.py
+++ members/urls.py
@@ -70,6 +70,7 @@ urlpatterns = [
     path("supprimer-mon-compte/", views.supprimer_compte_public, name="supprimer_compte"),
     path("mon-compte/supprimer/", views.delete_account, name="delete_account"),
     path("test-yuumi2/", views.testyuumi2, name="test-yuumi2"),
+    path("agent-ia/", views.ai_search_agent, name="ai_search_agent"),
 
 
     
